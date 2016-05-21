d338ee2a9d6cb1819e3ff58a99ef96596489c45d
吴文豪 <529280602@qq.com>
2016-05-21 10:01:57 +0800

    commit

--- graylog2-server/src/main/java/org/graylog2/bootstrap/ServerBootstrap.java
+++ graylog2-server/src/main/java/org/graylog2/bootstrap/ServerBootstrap.java
@@ -44,10 +44,8 @@ import org.slf4j.LoggerFactory;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -95,6 +93,7 @@ public abstract class ServerBootstrap extends CmdLineTool {
         LOG.info("Deployment: {}", configuration.getInstallationSource());
         LOG.info("OS: {}", os.getPlatformName());
         LOG.info("Arch: {}", os.getArch());
+
 
         final ServerStatus serverStatus = injector.getInstance(ServerStatus.class);
         serverStatus.initialize();
@@ -159,7 +158,7 @@ public abstract class ServerBootstrap extends CmdLineTool {
                 throw new Exception("Could not determine PID.");
             }
 
-            Files.write(pidFilePath, pid.getBytes(StandardCharsets.UTF_8), StandardOpenOption.WRITE, StandardOpenOption.CREATE_NEW, LinkOption.NOFOLLOW_LINKS);
+            Files.write(pidFilePath, pid.getBytes(StandardCharsets.UTF_8));
         } catch (Exception e) {
             LOG.error("Could not write PID file: " + e.getMessage(), e);
             System.exit(1);
